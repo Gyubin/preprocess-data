ba04f3171027fe4fe3acdbe406cac7cb1310f10a
Gyubin Son <geubin0414@gmail.com>
2019-06-15 19:10:02 +0000

    Add an execution file for resizing

---
+++ daewoo/01_resize_images.sh
@@ -0,0 +1,3 @@
+python ImageResizer.py --source_image_path="/path/to/the/raw_images"\
+                       --dest_image_path="/path/to/the/destination"\
+                       --crop_coords="350,250,1600,900"
