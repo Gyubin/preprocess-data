8d0599a8ac02e40103551c180cbd9d265dbcc098
Gyubin Son <geubin0414@gmail.com>
2019-06-24 20:22:58 +0000

    Modify execution file of making clips

--- daewoo/02_make_clips.sh
+++ daewoo/02_make_clips.sh
@@ -1,6 +1,54 @@
-python ClipMaker.py --source_image_path="/home/gyubin/Documents/korea_dsba/daewoo/weather/data/images_224_resize_rgb"\
-                    --dest_clip_path="clip_weather"\
-                    --image_label_path="/home/gyubin/Documents/korea_dsba/daewoo/weather/data/labels/image_label_matching_20190227.csv"\
-                    --clip_label_path="./clip_label.csv"\
+# Weather
+python ClipMaker.py --data_name="weather"\
+                    --source_image_path="/path/to/the/resized"\
+                    --dest_clip_path="/path/to/the/clips"\
+                    --image_label_path="/path/to/the/image/label"\
+                    --clip_label_path="/path/to/the/clip/label"\
+                    --frame_num=16\
+                    --frame_interval=1.1
+
+# Hyundai
+python ClipMaker.py --data_name="hyundai"\
+                    --source_image_path="/path/to/the/resized"\
+                    --dest_clip_path="/path/to/the/clips"\
+                    --image_label_path="/path/to/the/image/label"\
+                    --clip_label_path="/path/to/the/clip/label"\
                     --frame_num=16\
                     --frame_interval=2.0
+
+# LNGC
+python ClipMaker.py --data_name="lngc"\
+                    --source_image_path="/path/to/the/resized"\
+                    --dest_clip_path="/path/to/the/clips"\
+                    --image_label_path="/path/to/the/image/label"\
+                    --clip_label_path="/path/to/the/clip/label"\
+                    --frame_num=16\
+                    --frame_interval=3.0
+python ClipMaker.py --data_name="lngc"\
+                    --source_image_path="/path/to/the/resized"\
+                    --dest_clip_path="/path/to/the/clips"\
+                    --image_label_path="/path/to/the/image/label"\
+                    --clip_label_path="/path/to/the/clip/label"\
+                    --frame_num=16\
+                    --frame_interval=3.0
+python ClipMaker.py --data_name="lngc"\
+                    --source_image_path="/path/to/the/resized"\
+                    --dest_clip_path="/path/to/the/clips"\
+                    --image_label_path="/path/to/the/image/label"\
+                    --clip_label_path="/path/to/the/clip/label"\
+                    --frame_num=16\
+                    --frame_interval=3.0
+python ClipMaker.py --data_name="lngc"\
+                    --source_image_path="/path/to/the/resized"\
+                    --dest_clip_path="/path/to/the/clips"\
+                    --image_label_path="/path/to/the/image/label"\
+                    --clip_label_path="/path/to/the/clip/label"\
+                    --frame_num=16\
+                    --frame_interval=3.0
+python ClipMaker.py --data_name="lngc"\
+                    --source_image_path="/path/to/the/resized"\
+                    --dest_clip_path="/path/to/the/clips"\
+                    --image_label_path="/path/to/the/image/label"\
+                    --clip_label_path="/path/to/the/clip/label"\
+                    --frame_num=16\
+                    --frame_interval=3.0
