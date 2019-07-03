717d01548913c51bb2637d29e4ff93423edd7578
Gyubin Son <geubin0414@gmail.com>
2019-07-03 18:31:14 +0000

    Modify file path

--- daewoo/02_resize_images.sh
+++ daewoo/02_resize_images.sh
@@ -1,34 +1,36 @@
 # Weather
 python ImageResizer.py --data_name="weather"\
-                       --source_image_path="/path/to/the/raw_images"\
-                       --dest_image_path="/path/to/the/destination"\
+                       --source_image_path="/hdd1/daewoo/weather/raw_images"\
+                       --dest_image_path="/hdd1/daewoo/weather/resized"\
                        --crop_coords="350,250,1600,900"
 
 # Hyundai
 python ImageResizer.py --data_name="hyundai"\
-                       --source_image_path="/path/to/the/raw_images"\
-                       --dest_image_path="/path/to/the/destination"\
+                       --source_image_path="/hdd1/daewoo/hyundai/raw_images"\
+                       --dest_image_path="/hdd1/daewoo/hyundai/resized"\
                        --crop_coords="650,450,1600,750"\
-                       --hyundai_label="/path/to/the/hyundai/label"
+                       --hyundai_label="/home/gyubin/Documents/korea_dsba/daewoo/dataset/hyundai/labels/image_matching_waveparam_2min.csv"
 
 # LNGC
+LNGC_SRC="/hdd1/daewoo/lngc/raw_images"
+LNGC_DEST="/hdd1/daewoo/lngc/resized"
 python ImageResizer.py --data_name="lngc"\
-                       --source_image_path="/path/to/the/raw/cam1"\
-                       --dest_image_path="/path/to/the/dest/cam1"\
+                       --source_image_path="$LNGC_SRC/cam1"\
+                       --dest_image_path="$LNGC_DEST/cam1"\
                        --crop_coords="0,400,1000,600"
 python ImageResizer.py --data_name="lngc"\
-                       --source_image_path="/path/to/the/raw/cam2"\
-                       --dest_image_path="/path/to/the/dest/cam2"\
+                       --source_image_path="$LNGC_SRC/cam2"\
+                       --dest_image_path="$LNGC_DEST/cam2"\
                        --crop_coords="0,200,1200,400"
 python ImageResizer.py --data_name="lngc"\
-                       --source_image_path="/path/to/the/raw/cam3"\
-                       --dest_image_path="/path/to/the/dest/cam3"\
+                       --source_image_path="$LNGC_SRC/cam3"\
+                       --dest_image_path="$LNGC_DEST/cam3"\
                        --crop_coords="0,300,1600,550"
 python ImageResizer.py --data_name="lngc"\
-                       --source_image_path="/path/to/the/raw/cam4"\
-                       --dest_image_path="/path/to/the/dest/cam4"\
+                       --source_image_path="$LNGC_SRC/cam4"\
+                       --dest_image_path="$LNGC_DEST/cam4"\
                        --crop_coords="0,400,500,600"
 python ImageResizer.py --data_name="lngc"\
-                       --source_image_path="/path/to/the/raw/cam5"\
-                       --dest_image_path="/path/to/the/dest/cam5"\
+                       --source_image_path="$LNGC_SRC/cam5"\
+                       --dest_image_path="$LNGC_DEST/cam5"\
                        --crop_coords="0,300,1000,500"
