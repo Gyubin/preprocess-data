07fd1abb51de009bbd12318cc983f149019bc6cb
Gyubin Son <geubin0414@gmail.com>
2019-09-04 11:29:29 +0000

    Use for loop in shell script

--- daewoo/preprocess/03_make_clips.sh
+++ daewoo/preprocess/03_make_clips.sh
@@ -22,38 +22,13 @@ python ClipMaker.py --data_name="hyundai"\
                     --frame_interval=${FRAME_INTERVAL}
 
 # LNGC
-python ClipMaker.py --data_name="lngc"\
-                    --source_image_path="/hdd1/daewoo/lngc/resized/cam1"\
-                    --dest_clip_path="$DATASET_ROOT/lngc/clips_${FRAME_NUM}/cam1"\
-                    --image_label_path="$DATASET_ROOT/lngc/labels/image_label_cam1.csv"\
-                    --clip_label_path="$DATASET_ROOT/lngc/labels/clip${FRAME_NUM}_label_cam1.csv"\
-                    --frame_num=${FRAME_NUM}\
-                    --frame_interval=${FRAME_INTERVAL}
-python ClipMaker.py --data_name="lngc"\
-                    --source_image_path="/hdd1/daewoo/lngc/resized/cam2"\
-                    --dest_clip_path="$DATASET_ROOT/lngc/clips_${FRAME_NUM}/cam2"\
-                    --image_label_path="$DATASET_ROOT/lngc/labels/image_label_cam2.csv"\
-                    --clip_label_path="$DATASET_ROOT/lngc/labels/clip${FRAME_NUM}_label_cam2.csv"\
-                    --frame_num=${FRAME_NUM}\
-                    --frame_interval=${FRAME_INTERVAL}
-python ClipMaker.py --data_name="lngc"\
-                    --source_image_path="/hdd1/daewoo/lngc/resized/cam3"\
-                    --dest_clip_path="$DATASET_ROOT/lngc/clips_${FRAME_NUM}/cam3"\
-                    --image_label_path="$DATASET_ROOT/lngc/labels/image_label_cam3.csv"\
-                    --clip_label_path="$DATASET_ROOT/lngc/labels/clip${FRAME_NUM}_label_cam3.csv"\
-                    --frame_num=${FRAME_NUM}\
-                    --frame_interval=${FRAME_INTERVAL}
-python ClipMaker.py --data_name="lngc"\
-                    --source_image_path="/hdd1/daewoo/lngc/resized/cam4"\
-                    --dest_clip_path="$DATASET_ROOT/lngc/clips_${FRAME_NUM}/cam4"\
-                    --image_label_path="$DATASET_ROOT/lngc/labels/image_label_cam4.csv"\
-                    --clip_label_path="$DATASET_ROOT/lngc/labels/clip${FRAME_NUM}_label_cam4.csv"\
-                    --frame_num=${FRAME_NUM}\
-                    --frame_interval=${FRAME_INTERVAL}
-python ClipMaker.py --data_name="lngc"\
-                    --source_image_path="/hdd1/daewoo/lngc/resized/cam5"\
-                    --dest_clip_path="$DATASET_ROOT/lngc/clips_${FRAME_NUM}/cam5"\
-                    --image_label_path="$DATASET_ROOT/lngc/labels/image_label_cam5.csv"\
-                    --clip_label_path="$DATASET_ROOT/lngc/labels/clip${FRAME_NUM}_label_cam5.csv"\
-                    --frame_num=${FRAME_NUM}\
-                    --frame_interval=${FRAME_INTERVAL}
+for CAMERA_NUM in 1 2 3 4 5
+do
+    python ClipMaker.py --data_name="lngc"\
+                        --source_image_path="/hdd1/daewoo/lngc/resized/cam${CAMERA_NUM}"\
+                        --dest_clip_path="$DATASET_ROOT/lngc/clips_${FRAME_NUM}/cam${CAMERA_NUM}"\
+                        --image_label_path="$DATASET_ROOT/lngc/labels/image_label_cam${CAMERA_NUM}.csv"\
+                        --clip_label_path="$DATASET_ROOT/lngc/labels/clip${FRAME_NUM}_label_cam${CAMERA_NUM}.csv"\
+                        --frame_num=${FRAME_NUM}\
+                        --frame_interval=${FRAME_INTERVAL}
+done
